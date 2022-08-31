03acaac74d5af8e75337d8259f424c8ddd65d2b2
Peter <yangsh.info@gmail.com>
2022-09-01 07:12:14 +0800

    enhance the shell sort

--- src/main/java/sort/ShellSort.java
+++ src/main/java/sort/ShellSort.java
@@ -1,6 +1,38 @@
 package sort;
-//TODO
+
+import java.util.Arrays;
+
 public class ShellSort {
+
+    public int[] shellSort(int[] input){
+
+        int stepSize = input.length / 2;
+
+        int temp ;
+        while (stepSize > 0){
+            for (int i = stepSize  ; i< input.length;i++){
+                int j = i;
+                int num = input[j];
+                while ((j-stepSize) >= 0 && input[j-stepSize] > num ){
+                    input[j] = input[j - stepSize] ;
+                    j = j - stepSize;
+                }
+                input[j] = num;
+            }
+
+            stepSize = stepSize / 2;
+        }
+
+
+        return input;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new ShellSort().shellSort(new int[]{114, 131, 118, 85, 156, 147, 126, 15, 178, 159, 64, 93, 176, 141, 175, 63, 83, 123, 60, 44})));
+        System.out.println(Arrays.toString(new ShellSort().shellSort(new int[]{10,8,9,7,6,4,5,2,1})));
+        System.out.println(Arrays.toString(new ShellSort().shellSort(new int[]{1})));
+    }
+
 
 
 
